3908696c5b4a2211ba8204d11347995293b2278d
Kenan Blackerby <kblackerby2@gmail.com>
2020-11-19 21:11:30 -0600

    Finish tutorial by adding time travel

--- src/index.js
+++ src/index.js
@@ -13,48 +13,18 @@ function Square(props) {
 }
   
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        };
-    }
-
-    handleClick(i) {
-        // get a copy of the state
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        } 
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares, 
-            xIsNext: !this.state.xIsNext
-        });
-    }
-
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
   
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status = '';
-        if (winner) {
-            status = `Winner: ${winner}`;
-        } else {
-            status = `Next player: ${this.state.xIsNext? 'X' : 'O'}`;
-        }
-    
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -76,15 +46,76 @@ class Board extends React.Component {
 }
   
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            stepNumber: 0,
+            xIsNext: true
+        };
+    }
+
+    handleClick(i) {
+        // get a copy of the state
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        } 
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        })
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step,move) => {
+            const desc = move ? `Go to move #${move}` : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status = '';
+        if (winner === 'X' || winner === 'O') {
+            status = `Winner: ${winner}`;
+        } else if (winner === 'draw')
+            status = 'The game is a draw';
+        else {
+            status = `Next player: ${this.state.xIsNext? 'X' : 'O'}`;
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -110,12 +141,18 @@ function calculateWinner (sq) {
         [0,4,8],
         [2,4,6]
     ];
-
+    // winner
     for (let index = 0; index < lines.length; index++) {
         const [a, b, c] = lines[index];
         if (sq[a] && sq[a] === sq[b] && sq[a] === sq[c]) {
             return sq[a];
         } 
     }
+
+    // game is draw
+    if (!sq.includes(null)) {
+        return 'draw';
+    }
+
     return null;
 }
